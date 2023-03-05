b89c49bc0a75182707d377e8678cd2aea0b3ea89
JIM90mdp <juanignaciomascarenhas@gmail.com>
2023-03-04 21:00:14 -0300

    refactor/ deploy-managment

---
+++ prueba/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Shows from "./components/Shows";
+import NavBar from "./components/NavBar";
+import Shop from "./components/Shop";
+import Lessons from "./components/Lessons";
+import LandingPage from "./components/LandingPage/LandingPage";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Switch>
+        <Route exact path={"/"} component={LandingPage} />
+        <Route exact path={"/shows"} component={Shows} />
+        <Route exact path={"/Shop"} component={Shop} />
+        <Route exact path={"/Lessons"} component={Lessons} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
